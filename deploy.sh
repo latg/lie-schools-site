43394617554018814e360c4eeed1ee791822cd44
Alexander Perepechko <a@perep.ru>
2023-02-13 18:47:26 +0300

    up

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -r ./public/
 hugo
-rsync -rzp --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw -e ssh --progress --delete ./public/ fkn:/srv/lie-school.ru/
+rsync -rzp --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw -e ssh --progress --delete ./public/ lieschool:/srv/lie-school.ru/
