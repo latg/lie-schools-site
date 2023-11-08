6039be9d2c2fdff377b3d11afa735333a573f260
Alexander Perepechko <a@perep.ru>
2023-11-08 20:34:24 +0300

    removed .DS_Store

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 rm -r ./public/
 hugo
+rsync -rzp --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw -e ssh --progress --delete ./deploy/ lieschool:/opt/
+
 rsync -rzp --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw -e ssh --progress --delete ./public/ lieschool:/srv/lie-school.ru/
